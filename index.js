64c2e1b98a69cf257b6a9f83dffaeca25e7463ce
ManuelGodoyLucanas <mangodoy100@hotmail.com>
2023-05-04 12:19:41 -0400

    Finished test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let  currentUser = "Manuel";
+let  welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let  excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
